17e9ed7bd444beba21a0620e764c881bc05bb465
Kazumasa <catter121@gmail.com>
2017-09-16 03:23:13 +0900

    dev&deploy

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Emotion AI
+
+## Development
+
+```
+$ docker-compose up
+```
+
+### Deploy
+
+```
+$ heroku container:push web
+```
+
+
+<!-- Build a Docker image -->
+<!--  -->
+<!-- ``` -->
+<!-- docker build -t emotion-ai . -->
+<!-- ``` -->
+<!--  -->
+<!-- Run a Docker container -->
+<!--  -->
+<!-- ``` -->
+<!-- docker run -->
+<!-- ``` -->
--- webapp/app.py
+++ webapp/app.py
@@ -3,16 +3,20 @@ from sklearn import datasets, svm
 
 app = Flask(__name__)
 
-# Load Dataset from scikit-learn.
-digits = datasets.load_digits()
 
 @app.route('/')
 def hello():
+    # Load Dataset from scikit-learn.
+    digits = datasets.load_digits()
     clf = svm.SVC(gamma=0.001, C=100.)
     clf.fit(digits.data[:-1], digits.target[:-1])
     prediction = clf.predict(digits.data[-1:])
 
     return jsonify({'prediction': repr(prediction)})
 
+@app.route('/about')
+def about():
+    return 'about aaaaaaa'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
