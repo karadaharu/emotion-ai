b3207bed8ad9455f421df1e04793443d7817c54b
Kazumasa <catter121@gmail.com>
2017-09-20 20:20:01 +0900

    button click

--- webapp/app.py
+++ webapp/app.py
@@ -20,5 +20,9 @@ def hello():
 def about():
     return render_template('index.html')
 
+@app.route('/vote', methods=['GET', 'POST'])
+def vote():
+    return 'aaaa'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- webapp/static/js/init.js
+++ webapp/static/js/init.js
@@ -1,7 +1,11 @@
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
-
+    $('.btn-q').click( function () {
+      console.log('click');
+      $.post("/vote", function (result) {
+        console.log(result);
+      });
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
